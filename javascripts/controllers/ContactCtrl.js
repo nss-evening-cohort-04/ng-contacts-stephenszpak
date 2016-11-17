623dd9fe3b2947b0f79799a1d1cf9a9169101502
Stephen Szpak <srszpak@gmail.com>
2016-11-17 15:02:43 -0600

    working on layout most functionality is done

---
+++ javascripts/controllers/ContactCtrl.js
@@ -0,0 +1,36 @@
+"use strict";
+
+app.controller("ContactCtrl", function($scope, ContactFactory){
+	$scope.welcome = "hello";
+	$scope.newContact = {};
+	$scope.contacts = [];
+	$scope.showHide = true;
+
+
+	let gContacts = function() {
+		ContactFactory.getContacts().then(function(fbContact) {
+			$scope.contacts = fbContact;
+		});
+	};
+
+	gContacts();
+
+	console.log("contacts", $scope.contacts);
+		$scope.contactList = function() {
+			$scope.showHide = true;
+		};
+
+		$scope.moreInfo = function() {
+		$scope.showHide = false;
+	};
+
+	$scope.addContact = function() {
+		ContactFactory.addNewContact($scope.newContact).then(function(contactId) {
+			gContacts();
+			$scope.newContact = "";
+			});
+	};
+
+
+
+});
---
+++ javascripts/factories/ContactFactory.js
@@ -0,0 +1,49 @@
+"use strict";
+
+app.factory("ContactFactory", function($q, $http, FIREBASE_CONFIG) {
+
+	var getContacts = function() {
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json`)
+			.success(function(response) {
+				console.log(response);
+				let contacts = [];
+				Object.keys(response).forEach(function(key) {
+					response[key].id = key;
+					contacts.push(response[key]);
+				});
+				resolve(contacts);
+			})
+			.error(function(errorResponse) {
+				reject(errorResponse);
+			});
+		});
+	};
+
+	var addNewContact = function(newContact) {
+		return $q((resolve, reject) => {
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/contacts.json`,
+				JSON.stringify({
+					name: newContact.name,
+					phone: newContact.phone,
+					email: newContact.email,
+					url: newContact.url,
+					social: newContact.social,
+					relation: newContact.relation
+				})
+			)
+			.success(function(postResponse) {
+				resolve(postResponse);
+			})
+			.error(function(postError) {
+				reject(postError);
+			});
+		});
+	};
+
+
+	return {
+		getContacts : getContacts,
+		addNewContact : addNewContact
+	};
+});
